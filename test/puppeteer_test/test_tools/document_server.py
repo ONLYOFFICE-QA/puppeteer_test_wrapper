5f3957303f38c01135817c627a14be34ce60f15c
l02 <l8556@outlook.com>
2024-07-30 17:14:15 +0300

    Added dostrings to DocumentServer

--- test/puppeteer_test/test_tools/document_server.py
+++ test/puppeteer_test/test_tools/document_server.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import json
 import re
-from rich import print
 from typing import Optional
 from urllib.parse import urlparse
 
@@ -11,14 +9,25 @@ from .file_paths import FilePaths
 
 
 class DocumentServer:
+    """
+    A class to interact with a document server
+    """
     version_pattern = re.compile(r'Version:\s*(\d+\.\d+\.\d+)\s*\(build:(\d+)\)', re.IGNORECASE)
     sdk_link = 'sdkjs/word/sdk-all.js'
 
     def __init__(self, url: str):
+        """
+        Initialize the DocumentServer with a base URL.
+        :param url: The base URL of the document server.
+        """
         self.path = FilePaths()
         self.url = self._get_url(url)
 
     def get_version(self) -> Optional[str]:
+        """
+        Retrieve the version information from the SDK JavaScript file on the document server.
+        :return: The version string if found, otherwise None.
+        """
         if self.url:
             match = self.version_pattern.search(self._get_sdk_all_js_content(self.url))
             if match:
@@ -27,11 +36,21 @@ class DocumentServer:
 
     @staticmethod
     def _get_sdk_all_js_content(url: str) -> Optional[str]:
+        """
+        Retrieve the content of the SDK JavaScript file from the given URL.
+        :param url: The URL to the SDK JavaScript file.
+        :return: The content of the file if the request is successful, otherwise None.
+        """
         response = requests.get(url)
         if response.status_code == 200:
             return response.text
         return None
 
     def _get_url(self, url) -> str:
+        """
+        Construct the full URL to the SDK JavaScript file from the base URL.
+        :param url: The base URL of the document server.
+        :return: The full URL to the SDK JavaScript file.
+        """
         parsed_url = urlparse(url)
         return f"{parsed_url.scheme}://{parsed_url.netloc}/{self.sdk_link}"
